7ef290eea1bb9fd2f43272e9da20f74b344925c0
Michel khairallah <michelkhairallah67@outlook.com>
2024-05-08 13:16:06 +0300

    v4

--- src/pages/SignUpPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import SignUpForm from "../components/forms/SignUpForm"
-export default function SignUpPage() {
-  return (
-    <div>
-        <SignUpForm/>
-    </div>
-  )
-}
